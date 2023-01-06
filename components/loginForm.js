e56b09921c45e52972d08bf1a26e1a692733885b
Vlad <billymosh@gmail.com>
2023-01-06 18:47:38 +0100

    added coments,if have token>changing location to chat

--- components/loginForm.js
+++ components/loginForm.js
@@ -1,94 +1,88 @@
-isLoggedIn();
+const signUpBtn = document.querySelector("#registration");
+const signUpForm = document.querySelector(".signup-form");
+const signInForm = document.querySelector(".signin-form");
+const cancelBtn = document.querySelector("#cancel");
+const saveUserBtn = document.querySelector("#saveUser");
 
-const signUpBtn = document.querySelector("#registration")
-const signUpForm = document.querySelector(".signup-form")
-const signInForm = document.querySelector('.signin-form')
-const cancelBtn = document.querySelector("#cancel")
-const saveUserBtn = document.querySelector('#saveUser')
 
-signUpBtn.addEventListener('click', toggleFormMode)
-cancelBtn.addEventListener('click', toggleFormMode)
-signUpForm.addEventListener('submit', async (e) => {
-  e.preventDefault()
-  const password = signUpForm.password.value
-  const name = signUpForm.name.value
-  async function sendUser() {
-    try {
-      const response = await fetch('/api/signup', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-          name,
-          password
-        })
-      })
-    }
-    catch (error) {
-      console.log(error)
-    }
+
+signUpBtn.addEventListener("click", toggleFormMode);
+cancelBtn.addEventListener("click", toggleFormMode);
+signUpForm.addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const password = signUpForm.password.value;
+  const name = signUpForm.name.value;
+  try {
+    // Send a POST request to the server with the user data
+    const response = await fetch("/api/signup", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        name,
+        password,
+      }),
+    });
+  } catch (error) {
+    console.log(error);
   }
-  sendUser()
-  toggleFormMode()
-
-})
-signInForm.addEventListener('submit', async (e) => {
-  e.preventDefault()
-  const name = signInForm.name.value
-  const password = signInForm.password.value
-  async function sendUser() {
-    try {
-      const response = await fetch('/api/signin', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-          name,
-          password
-        })
-      });
-      if (response.ok) {
-        //  location.href = '/index.html'
-      }
+  toggleFormMode();
+});
+signInForm.addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const name = signInForm.name.value;
+  const password = signInForm.password.value;
+  try {
+    // Send a POST request to the server with the user data
+    const response = await fetch("/api/signin", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        name,
+        password,
+      }),
+    });
+    if (response.ok) {
+      // If the response is successful, redirect
+      // Redirect to the home page
+      location.href = "/index.html";
     }
-    catch (error) {
-      console.log(error)
-    }
+  } catch (error) {
+    console.log(error);
   }
-  sendUser()
-})
+});
 
+// Toggle between the sign up and sign in forms
 function toggleFormMode() {
-  signUpForm.hidden = !signUpForm.hidden
-  signInForm.hidden = !signInForm.hidden
+  signUpForm.hidden = !signUpForm.hidden;
+  signInForm.hidden = !signInForm.hidden;
 }
 
+// Check if the user is logged in
 async function isLoggedIn() {
-  const cookie = document.cookie;
-  const regex = /(?:^| )token=([^;]+)/;
-  const match = cookie.match(regex);
-  const token = match ? match[1] : null;
-
-  if (!token) {
-    location.href = '/login.html';
-    return;
-  }
-
-  const response = await fetch("/api/token", {
-    headers: {
-      Cookie: `token=${token}`
-    }
-  });
+  // Check if the 'token' cookie is present in the request header
+  const token = getToken();
+  if (!token) return; // Return if the cookie is not present
+  // Send a GET request to the server to check for a valid token cookie
+  const response = await fetch("/api/token");
   if (response.status !== 200) {
-    location.href = '/index.html';
+    // If the token is invalid, redirect to the login page
+    location.href = "/login.html";
+  }
+  if (response.ok) {
+    // If the token is valid, redirect to the home page
+    location.href = "/index.html";
   }
 }
+function getToken() {
+  const cookie = document.cookie;
+  if (!cookie) return null;
+  const tokenMatch = cookie.match(/token=([^;]+)/);
+  if (!tokenMatch) return null;
+  return tokenMatch[1];
+}
 
-
-
-
-
-
-
+isLoggedIn();
--- index.js
+++ index.js
@@ -1,35 +1,46 @@
-const http = require("http")
-const fs = require('fs')
-const blacklist = require('./blacklist')
-const port = 3000
-const server = http.createServer(handleRequest)
-server.listen(port, () => {
-  console.log(`server is on http://localhost:${port}`)
-})
+const http = require("http");
+const fs = require("fs");
+const blacklist = require("./blacklist");
 
+const port = 3000;
+const server = http.createServer(handleRequest);
+server.listen(port, () => {
+  console.log(`server is listening on http://localhost:${port}`);
+});
+
+// Generate a unique ID for a new user
 function generateId() {
   const timestamp = Date.now();
   const randomNumber = Math.random();
   return `${timestamp}-${randomNumber}`;
 }
 
+// Parse the JSON data from the 'db.json' and 'users.json' files
+const db = JSON.parse(fs.readFileSync("./db.json", "utf8"));
+const users = JSON.parse(fs.readFileSync("./users.json", "utf8"));
 
-
-const db = JSON.parse(fs.readFileSync('./db.json', 'utf8'))
-const users = JSON.parse(fs.readFileSync('./users.json', 'utf8'))
-const maxAge = 60000
-
+// The maximum age of the token cookie in milliseconds
+const maxAge = 60000;
 
 async function handleRequest(request, response) {
-  const {method, url} = request
+  // Destructure the request method and URL
+  const { method, url } = request;
 
+  // Handle POST requests
   if (method === "POST") {
     if (url === "/api/data") {
       try {
+        // Get the request body
         const body = await getBody(request);
+
+        // Parse the JSON data in the request body
         const newMessage = JSON.parse(body);
+
+        // Add the new message to the 'db.json' file
         db.push(newMessage);
         fs.writeFileSync("db.json", JSON.stringify(db));
+
+        // Send a response to the client
         response.end("OK\n");
       } catch (error) {
         console.error(error);
@@ -38,31 +49,56 @@ async function handleRequest(request, response) {
       }
     } else if (url === "/api/signup") {
       try {
+        // Get the request body
         const body = await getBody(request);
+
+        // Parse the JSON data in the request body
         const newUser = JSON.parse(body);
-        newUser.id = generateId()
+
+        // Generate an ID for the new user
+        newUser.id = generateId();
+
+        // Add the new user to the 'users.json' file
         users.push(newUser);
         fs.writeFileSync("users.json", JSON.stringify(users));
+
+        // Send a response to the client
         response.end("OK\n");
       } catch (error) {
         console.error(error);
         response.statusCode = 500;
         response.end("Internal Server Error\n");
       }
-    } else if (url === '/api/signin') {
+    } else if (url === "/api/signin") {
       try {
+        // Get the request body
         const body = await getBody(request);
-        const checkedUser = JSON.parse(body)
+
+        // Parse the JSON data in the request body
+        const checkedUser = JSON.parse(body);
+
+        // Find the user in the 'users.json' file
         const foundUser = users.find(
-          user => user.name === checkedUser.name
-          && user.password === checkedUser.password
-        )
+          (user) =>
+            user.name === checkedUser.name &&
+            user.password === checkedUser.password
+        );
+
         if (foundUser) {
-          const token = foundUser.id
-          response.setHeader("Set-Cookie", `token=${token}; Max-Age=${maxAge}; Path=/ ` );
+          // If the user is found
+          // Generate a token for the user
+          const token = foundUser.id;
+
+          // Set the token cookie in the response header
+          response.setHeader(
+            "Set-Cookie",
+            `token=${token}; Max-Age=${maxAge}; Path=/ `
+          );
+
+          // Send an "OK" response to the client
           response.end("OK\n");
-        }
-        else {
+        } else {
+          // If the user is not found, send a "Unauthorized" response
           response.statusCode = 401;
           response.end("Unauthorized\n");
         }
@@ -73,8 +109,10 @@ async function handleRequest(request, response) {
       }
     }
   } 
+  // Handle GET requests
   else if (method === "GET") {
     if (url === "/api/data") {
+      // Read the 'db.json' file and send its contents to the client
       let data;
       try {
         data = fs.readFileSync("db.json");
@@ -83,28 +121,39 @@ async function handleRequest(request, response) {
       }
       response.statusCode = 200;
       response.end(data);
-    }
-    else if (url === '/api/token') {
+    } else if (url === "/api/token") {
+      // Get the token from the request header
       const token = getToken(request);
-      const foundUser = users.find(user => user.id === token);
+
+      // Find the user with a matching ID in the 'users.json' file
+      const foundUser = users.find((user) => user.id === token);
+
       if (foundUser) {
+        // If the user is found, send an "OK" response
         response.end("OK\n");
       } else {
+        // If the user is not found, send a "Unauthorized" response
         response.statusCode = 401;
         response.end("Unauthorized\n");
-      }}
-    else {
+      }
+    } else {
+      // Handle requests for static files
       if (url === "/") {
+        // Read the 'index.html' file and send its contents to the client
         const html = fs.readFileSync("index.html");
         response.end(html);
         return;
       }
       try {
+        // If the requested file is blacklisted, throw an error
         if (blacklist.includes(url)) throw null;
+
+        // Read the requested file and send its contents to the client
         const file = fs.readFileSync(url.slice(1));
         response.end(file);
       } catch (error) {
-        response.statusCode = 404; // Not found
+        // If the file is not found, send a "404 Not found" response
+        response.statusCode = 404;
         response.end(
           `<div>Error, ${url.slice(
             1
@@ -115,25 +164,30 @@ async function handleRequest(request, response) {
   }
 }
 
-//save data in server from client
-function getBody(req) {
+// Get the request body as a string
+async function getBody(request) {
   return new Promise((resolve, reject) => {
-    let body = ""
-    req.on('data', chunk => {
-      body += chunk.toString()
-    })
-    req.on('end', () => {
-      resolve(body)
-    })
-    req.on('error', error => {
-      reject(error)
-    })
-  })
+    let body = "";
+    request
+      .on("data", (chunk) => {
+        body += chunk.toString();
+      })
+      .on("end", () => {
+        resolve(body);
+      })
+      .on("error", (error) => {
+        reject(error);
+      });
+  });
 }
 
-function getToken(request){
+// Get the 'token' cookie from the request header
+function getToken(request) {
   const cookie = request.headers.cookie;
-  const regex = /(?:^| )token=([^;]+)/;
-  const match = cookie.match(regex);
-  return match
+  if (!cookie) return null;
+  const tokenMatch = cookie.match(/token=([^;]+)/);
+  if (!tokenMatch) return null;
+  return tokenMatch[1];
 }
+
+    
